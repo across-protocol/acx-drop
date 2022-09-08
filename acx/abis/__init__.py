da23fbe8b20217a067ad268713e5f849813a0462
Chase Coleman <cc7768@gmail.com>
2022-09-08 10:16:30 -0500

    feat: Add ABI subpackage (#2)

---
+++ acx/abis/__init__.py
@@ -0,0 +1,27 @@
+import json
+import pkgutil
+
+
+# Mapping of ABI names to filenames
+ABIs = {
+    "BridgePool": "v1BridgePool.json",
+    "HubPool": "v2HubPool.json"
+}
+
+
+def getABI(x: str):
+    """
+    Retrieve an ABI from the acx.abis folder using its contract name
+
+    Available ABIs are:
+
+    * "ERC20"
+    * "BridgePool"
+    * "HubPool"
+    """
+    if x in ABIs.keys():
+        filename = ABIs[x]
+    else:
+        raise ValueError("Only certain ABIs available. Rtfd")
+
+    return json.loads(pkgutil.get_data("acx.abis", filename))
