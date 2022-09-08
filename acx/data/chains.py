59cbf541dc78ee2afffb769325a7304579c2551a
Chase Coleman <cc7768@gmail.com>
2022-09-08 10:17:20 -0500

    feat: Add chain/token data (#3)

---
+++ acx/data/chains.py
@@ -0,0 +1,20 @@
+import json
+import pkgutil
+
+
+# Load the chain list
+CHAINLIST = json.loads(
+    pkgutil.get_data("acx.data", "chains.json")
+)
+
+
+#
+# Use the chainlist to generate helpful mappings
+#
+ID_TO_NAME = {
+    x["chainId"]: x["chainName"] for x in CHAINLIST
+}
+
+ID_TO_SHORTNAME = {
+    x["chainId"]: x["shortName"] for x in CHAINLIST
+}
