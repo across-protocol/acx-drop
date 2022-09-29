6488269d9ab7bf84b4d2d75f4cfd3d6c9a6d3191
Chase Coleman <cc7768@gmail.com>
2022-09-29 19:37:32 +0000

    Update wt_stargate.py
    Co-authored-by: James Morris <96435344+james-a-morris@users.noreply.github.com>

--- wt_stargate.py
+++ wt_stargate.py
@@ -96,9 +96,10 @@ if __name__ == "__main__":
                 continue
 
             # Get pool address and first/last block from params
-            poolAddress = contractInfo[chainId][token]["address"]
-            fb = contractInfo[chainId][token]["first_block"]
-            lb = contractInfo[chainId][token]["last_block"]
+            poolContract = contractInfo[chainId][token]
+            poolAddress = poolContract["address"]
+            fb = poolContract["first_block"]
+            lb = poolContract["last_block"]
 
             # Create pool
             pool = w3.eth.contract(
