5874703dbbcc6f5ef519902cee61ad27b0f1f266
Chase Coleman <cc7768@gmail.com>
2022-10-04 14:49:35 +0000

    Check for missing values before merge

--- combine_rewards.py
+++ combine_rewards.py
@@ -5,16 +5,22 @@ bridgoor = pd.DataFrame(
     pd.read_json("final/bridgoor_rewards.json", typ="series"),
     columns=["bridgoor"]
 )
+if bridgoor["bridgoor"].isna().any():
+    raise ValueError("Missing values in bridgoor data")
 
 lp = pd.DataFrame(
     pd.read_json("final/lp_rewards.json", typ="series"),
     columns=["lp"]
 )
+if lp["lp"].isna().any():
+    raise ValueError("Missing values in lp data")
 
 travelers = pd.DataFrame(
     pd.read_json("final/traveler_rewards.json", typ="series"),
     columns=["traveler"]
 )
+if travelers["traveler"].isna().any():
+    raise ValueError("Missing values in traveler data")
 
 out = (
     pd.DataFrame(travelers).join(
