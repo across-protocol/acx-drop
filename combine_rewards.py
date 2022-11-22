30e959edb802ac1c25f0a11ae7990c8964b53871
Chase Coleman <cc7768@gmail.com>
2022-11-22 19:42:45 +0000

    feat: Update combine script and add final files

--- combine_rewards.py
+++ combine_rewards.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 
 
@@ -22,9 +24,14 @@ travelers = pd.DataFrame(
 if travelers["bridge-traveler"].isna().any():
     raise ValueError("Missing values in traveler data")
 
+community = pd.DataFrame(
+    pd.read_json("final/community_rewards.json", typ="series"),
+    columns=["community"]
+)
+
 out = (
     pd.DataFrame(travelers).join(
-        [bridgoor, lp], how="outer"
+        [bridgoor, community, lp], how="outer"
     )
     .fillna(0.0)
 )
@@ -33,3 +40,32 @@ out.index.name = "address"
 
 out.to_json("final/final_combined.json", orient="index")
 out.to_csv("final/final_combined.csv")
+
+# Format for mr
+mrout = {}
+zeros = "0"*18
+
+mrout["recipients"] = []
+for (address, row) in out.iterrows():
+    entry = {
+        "account": address,
+        "amount": str(int(row["total"] * 1e18)),
+        "metadata": {
+            "amountBreakdown": {
+                "communityRewards":  str(int(row["community"] * 1e18)),
+                "welcomeTravelerRewards": str(int(row["bridge-traveler"] * 1e18)),
+                "earlyUserRewards": str(int(row["bridgoor"] * 1e18)),
+                "liquidityProviderRewards": str(int(row["lp"] * 1e18))
+            }
+        }
+    }
+    mrout["recipients"].append(entry)
+
+# Metadata
+mrout["chainId"] = 1
+mrout["rewardToken"] = ""
+mrout["windowIndex"] = 0
+mrout["rewardsToDeposit"] = str(int(out["total"].sum() * 1e18))
+
+with open("final/mr.json", "w") as f:
+    json.dump(mrout, f)
